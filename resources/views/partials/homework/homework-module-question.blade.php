bb9eb23f550008ac79d5964854bf0cc1c3d98063
Che Ming Yang <chemingyang@hotmail.com>
2025-10-21 18:50:14 -0400

    ok

--- resources/views/partials/homework/homework-module-question.blade.php
+++ resources/views/partials/homework/homework-module-question.blade.php
@@ -25,16 +25,16 @@
         });
     </script>
 @elseif (@$type === 'drop')
-    <div class="grid w-full gap-6 md:grid-cols-2">
-        <div id="sort-1-left" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 justify-left h-full" >
+    <!--<div class="grid w-full gap-6 md:grid-cols-2"> -->
+        <div id="sort-1-left" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 justify-left min-h-18 mt-2" >
             <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span class="p-1">item 1</span></div>
             <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span class="p-1">item 2</span></div>
             <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span class="p-1">item 3</span></div>
         </div>
-        <div id="sort-1-right" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 h-full w-full space-x-1 justify-center">
+        <div id="sort-1-right" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 h-full w-full space-x-1 justify-center min-h-18 mt-2">
             <span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">box1</span>
         </div>
-    </div>
+    <!-- </div> -->
     <script>
         document.addEventListener('DOMContentLoaded', () => {
             elemIDArr = ['sort-1-left','sort-1-right'];
@@ -51,21 +51,21 @@
         });
     </script>
 @elseif (@$type === 'match')
-    <div class="grid w-full gap-6 md:grid-cols-2">
+    <!--<div class="grid w-full gap-6 md:grid-cols-2"> -->
         <div>
-            <div id="sort-2-left" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 justify-left h-full">
+            <div id="sort-2-left" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 justify-left min-h-18 mt-2">
                 <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span>item 1</span></div>
                 <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span>item 2</span></div>
                 <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span>item 3</span></div>
             </div>
         </div>
         <div class="flex justify-right">
-            <div id="sort-2-right-1" data-val="{{ $idx }}" data-rel="" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 mr-2 h-full w-full space-x-1 justify-center"><span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">box1</span></div>
-            <div id="sort-2-right-2" data-val="{{ $idx }}" data-rel="" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 mr-2 h-full w-full space-x-1 justify-center"><span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">box2</span></div>
-            <div id="sort-2-right-3" data-val="{{ $idx }}" data-rel="" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 mr-2 h-full w-full space-x-1 justify-center"><span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">box3</span></div>
+            <div id="sort-2-right-1" data-val="{{ $idx }}" data-rel="" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 mr-2 h-full w-full space-x-1 justify-center min-h-18 mt-2"><span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">box1</span></div>
+            <div id="sort-2-right-2" data-val="{{ $idx }}" data-rel="" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 mr-2 h-full w-full space-x-1 justify-center min-h-18 mt-2"><span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">box2</span></div>
+            <div id="sort-2-right-3" data-val="{{ $idx }}" data-rel="" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 mr-2 h-full w-full space-x-1 justify-center min-h-18 mt-2"><span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">box3</span></div>
         </div>
-    </div>
-    <script>/*
+    <!-- </div> -->
+    <script>
         document.addEventListener('DOMContentLoaded', () => {
             elemIDArr = ['sort-2-left'];
             elemIDArr.forEach(function(elemID, index) {
@@ -94,7 +94,7 @@
                     ghostClass: 'blue-background-class'
                 });
             });
-        }); */
+        }); 
     </script>
 @else
     <span>invalid question type</span>
