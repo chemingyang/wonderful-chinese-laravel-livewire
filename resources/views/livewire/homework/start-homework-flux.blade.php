60ae15c64293af764c3f395573642cf1a40f6ec5
Che Ming Yang <chemingyang@hotmail.com>
2025-10-22 12:49:18 -0400

    clean up the html

--- resources/views/livewire/homework/start-homework-flux.blade.php
+++ resources/views/livewire/homework/start-homework-flux.blade.php
@@ -8,7 +8,7 @@
         <flux:heading size="lg">{{ $lessonmodule->prompt }} </flux:text>
     </div>
     <div class="space-y-6 p-3">
-        @include('partials.homework.homework-module-question',['type' => $lessonmodule->type, 'question' => $lessonmodule->question, 'idx' => $index + 1 ])
+        @include('partials.homework.homework-module-question',['type' => $lessonmodule->type, 'question' => $lessonmodule->question, 'index' => $index + 1, 'rel' => $lessonmodule->id])
     </div>
     <flux:separator class="my-4"/>
     @empty
@@ -18,7 +18,26 @@
         <flux:button variant="primary" class="w-2xs float-end">Next</flux:button>
     </div>
 <div>
-</section>
+<div>
+    <span>hidden form not supposed to see this</span>
+    <form method="POST" wire:submit="store">
+        @foreach (@$lessonmodules as $key => $lessonmodule)
+            <flux:input
+                class="answers"
+                id="a{{$key}}"
+                {{-- wire:model="form.answers.{{$lessonmodule->id}}" --}}
+                data-question="{{ $lessonmodule->question }}"
+                data-type="{{ $lessonmodule->type }}"
+                data-id="{{$lessonmodule->id}}"
+                type="text"
+                placeholder="your answer"
+            />
+        @endforeach
+        <flux:input id="student-id" type="text" wire:model="form.student_id" placeholder="student id" />
+        <flux:input id="lesson-id" type="text" wire:model="form.lesson_id" placeholder="lesson id"/>
+        <button id="submit-button" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-md px-8 py-4 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit Homework</button>
+    </form>
+</div>
 <script>
     /*
     function initSort(elemIDArr) {
@@ -36,6 +55,7 @@
     document.addEventListener('DOMContentLoaded', () => {
         // console.log('DOM is loaded! livewire initialized');
         //document.getElementById('start-homework').show();
-    });
+    });</section>
     */
 </script>
+</section>
--- resources/views/partials/homework/homework-module-question.blade.php
+++ resources/views/partials/homework/homework-module-question.blade.php
@@ -1,43 +1,22 @@
 <flux:fieldset>
 @if (@$type === 'fill-in-blank')
-    <div>{!! str_replace('<>','<input type="text" class="inline border-1 border-color:#fff" style="width:80px; padding:5px; margin:5px" name="" data-rel="a" />',$question); !!}</div>
+    <div>Q{{ ($index) }}.{!! str_replace('<>','<input type="text" class="inline border-1 border-color:#fff" style="width:80px; padding:5px; margin:5px" name="" data-rel="{{$rel}}" />',$question); !!}</div>
 @elseif (@$type === 'answer-question')
-    <flux:text class="mt-2" size="md">Q{{ $idx }}. {{ $question }} </flux:text>
-    <flux:textarea rows="10" columns="35" label="Answer:" />
+    <label>Q{{ ($index) }}. {{ $question }}</label>
+    <flux:textarea rows="10" columns="35" data-rel="{{$rel}}" />
 @elseif (@$type === 'sort')
-    <div class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 mt-2 min-h-18" id="sort0">
-        <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span>1. item 1</span></div>
-        <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span>2. item 2</span></div>
-        <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span>3. item 3</span></div>
-        <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span>4. item 4</span></div>
+    @php
+        $words = explode('|',$question);
+        $sorts = ['sort-'.$index];
+    @endphp
+    <div id="{{$sorts[0]}}" data-rel="{{$rel}}" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 mt-2 min-h-18" id="sort0">
+    @foreach ($words as $i => $word)
+        <div data-val="{{($i+1)}}" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span>{{$word}}</span></div>
+    @endforeach
     </div>
     <script>
         document.addEventListener('DOMContentLoaded', () => {
-            let elemID = 'sort0';
-            let el = document.getElementById(elemID);
-            new Sortable(el, {
-                animation: 150,
-                group: {
-                    name: 'shared'
-                },
-                ghostClass: 'blue-background-class'
-            });
-        });
-    </script>
-@elseif (@$type === 'drop')
-    <!--<div class="grid w-full gap-6 md:grid-cols-2"> -->
-        <div id="sort-1-left" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 justify-left min-h-18 mt-2" >
-            <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span class="p-1">item 1</span></div>
-            <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span class="p-1">item 2</span></div>
-            <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span class="p-1">item 3</span></div>
-        </div>
-        <div id="sort-1-right" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 h-full w-full space-x-1 justify-center min-h-18 mt-2">
-            <span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">box1</span>
-        </div>
-    <!-- </div> -->
-    <script>
-        document.addEventListener('DOMContentLoaded', () => {
-            elemIDArr = ['sort-1-left','sort-1-right'];
+            elemIDArr = {!! json_encode( $sorts) !!};
             elemIDArr.forEach(function(elemID, index) {
                 let el = document.getElementById(elemID);
                 new Sortable(el, {
@@ -50,24 +29,66 @@
             });
         });
     </script>
+@elseif (@$type === 'drop')
+    @php
+        $qparts = explode(':',$question);
+        $prompt = $qparts[0];
+        $words = explode('|',$qparts[1]);
+        $sorts = ['sort-'.$index.'-left','sort-'.$index.'-right'];
+    @endphp
+    <!--<div class="grid w-full gap-6 md:grid-cols-2"> -->
+        <div id="{{$sorts[0]}}" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 justify-left min-h-18 mt-2" >
+        @foreach ($words as $i => $word)
+            <div data-val="{{($i+1)}}" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span class="p-1">{{$word}}</span></div>
+        @endforeach
+        </div>
+        <div id="{{$sorts[1]}}" data-rel="{{$rel}}" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 h-full w-full space-x-1 justify-center min-h-18 mt-2">
+            <span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">{{$prompt}}</span>
+        </div>
+    <!-- </div> -->
+    <script> /*
+        document.addEventListener('DOMContentLoaded', () => {
+            elemIDArr = {!! json_encode($sorts) !!};
+            elemIDArr.forEach(function(elemID, index) {
+                let el = document.getElementById(elemID);
+                new Sortable(el, {
+                    animation: 150,
+                    group: {
+                        name: 'shared'
+                    },
+                    ghostClass: 'blue-background-class'
+                });
+            });
+        });*/
+    </script>
 @elseif (@$type === 'match')
+    @php
+        $qparts = explode(':',$question);
+        $words = explode('|',$qparts[0]);
+        $boxes = explode('|',$qparts[1]);
+        $sortsleft = ['sort-'.$index.'-left'];
+        $sortsright = [];
+    @endphp
     <!--<div class="grid w-full gap-6 md:grid-cols-2"> -->
         <div>
-            <div id="sort-2-left" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 justify-left min-h-18 mt-2">
-                <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span>item 1</span></div>
-                <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span>item 2</span></div>
-                <div data-val="" data-rel="" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span>item 3</span></div>
+            <div id="{{$sortsleft[0]}}" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 justify-left min-h-18 mt-2">
+            @foreach($words as $i => $word)
+                <div data-val="{{($i+1)}}" class="list-group-item focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-4 m-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" style="z-index:1; opacity:75%;"><span>{{$word}}</span></div>
+            @endforeach
             </div>
         </div>
         <div class="flex justify-right">
-            <div id="sort-2-right-1" data-val="{{ $idx }}" data-rel="" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 mr-2 h-full w-full space-x-1 justify-center min-h-18 mt-2"><span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">box1</span></div>
-            <div id="sort-2-right-2" data-val="{{ $idx }}" data-rel="" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 mr-2 h-full w-full space-x-1 justify-center min-h-18 mt-2"><span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">box2</span></div>
-            <div id="sort-2-right-3" data-val="{{ $idx }}" data-rel="" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 mr-2 h-full w-full space-x-1 justify-center min-h-18 mt-2"><span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">box3</span></div>
+            @foreach($boxes as $i => $box)
+            @php
+                $sortsright[] = 'sort-'.$index.'-right'.$i;
+            @endphp
+                <div id="{{$sortsright[$i]}}" data-rel="{{$rel}}" class="flex list-group border border-gray-200 rounded-lg cursor-pointer p-1 mr-2 h-full w-full space-x-1 justify-center min-h-18 mt-2"><span style="position:absolute; opacity: 50%;" class="px-6 py-0 filtered">{{$box}}</span></div>
+            @endforeach
         </div>
     <!-- </div> -->
-    <script>
+    <script>/*
         document.addEventListener('DOMContentLoaded', () => {
-            elemIDArr = ['sort-2-left'];
+            elemIDArr = {!! json_encode($sortsleft) !!};
             elemIDArr.forEach(function(elemID, index) {
                 let el = document.getElementById(elemID);
                 new Sortable(el, {
@@ -79,7 +100,7 @@
                     ghostClass: 'blue-background-class'
                 });
             });
-            elemIDArr = ['sort-2-right-1','sort-2-right-2','sort-2-right-3'];
+            elemIDArr = {!! json_encode($sortsright) !!};
             elemIDArr.forEach(function(elemID, index) {
                 let el = document.getElementById(elemID);
                 new Sortable(el, {
@@ -94,7 +115,7 @@
                     ghostClass: 'blue-background-class'
                 });
             });
-        }); 
+        }); */
     </script>
 @else
     <span>invalid question type</span>
