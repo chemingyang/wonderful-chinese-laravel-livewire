34f548d1b3332d0557ef8131bf1bb67ee15a4534
Che Ming Yang <chemingyang@hotmail.com>
2025-10-23 15:10:38 -0400

    hide sections and enable next and prev buttons

--- resources/views/livewire/homework/start-homework-flux.blade.php
+++ resources/views/livewire/homework/start-homework-flux.blade.php
@@ -3,27 +3,30 @@
     <flux:heading size="xl">{{ @$lesson->title }}</flux:heading>
 </div>
 <div id="static-modal" data-modal-backdrop="static" tabindex="-1" class="overflow-y-auto overflow-x-hidden top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 max-w-3xl max-h-full">
+    @php
+        $moduleCount = count($lessonmodules);
+    @endphp
     @forelse (@$lessonmodules as $index => $lessonmodule)
-    <div class="space-y-6 p-3">
+    <div id="prompt-{{$index}}" class="space-y-6 p-3 section hidden">
         <flux:heading size="lg">{{ $lessonmodule->prompt }} </flux:text>
     </div>
-    <div class="space-y-6 p-3">
+    <div id="qa-{{$index}}" class="space-y-6 p-3 section hidden">
         @include('partials.homework.homework-module-question',['type' => $lessonmodule->type, 'question' => $lessonmodule->question, 'index' => $index, 'rel' => $lessonmodule->id])
     </div>
-    <flux:separator class="my-4"/>
     @empty
     @endforelse
+    <flux:separator class="my-4"/>
     <div class="space-y-6 p-3">
-        <flux:button variant="ghost" class="w-2xs">Previous</flux:button>
-        <flux:button variant="primary" class="w-2xs float-end">Next</flux:button>
+        <flux:button id="previous-btn" variant="ghost" class="w-2xs" data-incr="-1">Previous</flux:button>
+        <flux:button id="next-btn" variant="primary" class="w-2xs float-end" data-incr="1">Next</flux:button>
     </div>
-<div>
+</div>
 <div>
     <span>hidden form not supposed to see this</span>
     <form method="POST" wire:submit="store">
         @foreach (@$lessonmodules as $key => $lessonmodule)
             <flux:input
-                class="answers"
+                class="answers hidden"
                 id="a{{$key}}"
                 wire:model="form.answers.{{$lessonmodule->id}}"
                 data-question="{{ $lessonmodule->question }}"
@@ -32,13 +35,54 @@
                 type="text"
                 placeholder="your answer"
             />
-        @endforeach let data_rel = document.getElementById('a'.idx);
-        <flux:input id="student-id" type="text" wire:model="form.student_id" placeholder="student id" />
-        <flux:input id="lesson-id" type="text" wire:model="form.lesson_id" placeholder="lesson id"/>
-        <button id="submit-button" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-md px-8 py-4 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit Homework</button>
+        @endforeach
+        <flux:input id="student-id" type="text" wire:model="form.student_id" class="hidden" />
+        <flux:input id="lesson-id" type="text" wire:model="form.lesson_id" class="hidden"/>
+        <button id="submit-button" type="submit" class="hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-md px-8 py-4 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit Homework</button>
     </form>
-
-
-
 </div>
+<script>
+    var currentIndex = null;
+
+    function setSection(currentidx) {
+        for (const section of document.getElementById('static-modal').getElementsByClassName('section')) {
+            section.classList.add('hidden');
+        }
+        document.getElementById('prompt-'+currentidx).classList.remove('hidden');
+        document.getElementById('qa-'+currentidx).classList.remove('hidden');
+
+        /* add some visual cues to the previous and next button when reaching edges */
+
+        /* if (currentIndex == 0) {
+            document.getElementById('previous-btn').classList.add('hidden');
+            document.getElementById('next-btn').classList.remove('hidden');
+        } else if (currentIndex == 4) {
+            document.getElementById('previous-btn').classList.remove('hidden');
+            document.getElementById('next-btn').classList.add('hidden');
+        } else {
+            document.getElementById('previous-btn').classList.remove('hidden');
+            document.getElementById('next-btn').classList.remove('hidden');
+        }*/
+    }
+
+    function handleClick(evt) {
+        let incr = this.getAttribute('data-incr');
+        let prevIndex = currentIndex;
+        let moduleCount = {{$moduleCount}};
+        currentIndex += parseInt(incr);
+        if (currentIndex >= 0 && currentIndex < moduleCount) {
+            setSection(currentIndex);
+        } else {
+            currentIndex = prevIndex;
+        }
+    }
+
+    document.getElementById('previous-btn').addEventListener('click', handleClick);
+    document.getElementById('next-btn').addEventListener('click', handleClick);
+
+    document.addEventListener('DOMContentLoaded', () => {
+        currentIndex = 0;
+        setSection(currentIndex);
+    });
+</script>
 </section>
