6f8f6c55245b6466d3fd2305270b0432cd9168ee
Che Ming Yang <chemingyang@hotmail.com>
2025-10-30 09:28:46 -0400

    teacher comment box should be okay

--- resources/views/partials/homework/homework-teacher-comment-js.blade.php
+++ resources/views/partials/homework/homework-teacher-comment-js.blade.php
@@ -9,34 +9,38 @@
         commentList[i] = document.getElementById(commentListID[i]);
         commentDiv[i] = document.getElementById(commentDivID[i]);
 
-        //console.log(matched);
         if (matched) {
-            //console.log(commentList[i].querySelectorAll('li'));
             commentArea[i].value = commentList[i].querySelectorAll('li')[0].innerText;
             setFormVal(commentArea[i]);
         }
 
         commentArea[i].addEventListener('focus', function(event) {
-            //if (commentArea[i].value == null || commentArea[i].value == "") {
+            const lists = document.getElementsByClassName('commentList');
+            for (const list of lists) {
+                list.classList.add('hidden');
+            }
             commentList[i].classList.remove('hidden');
-            //}
         });
 
         /* cannot seem to get this to work properly*/
         /* teacher forced to select one or type something */
-        commentList[i].addEventListener('blur', function(event) {
-            console.log('blur...');
+        commentDiv[i].addEventListener('blur', function(event) {
+            //console.log('blur...');
             //if (commentArea[i].value == null || commentArea[i].value == "") {
             commentList[i].classList.add('hidden');
             //}
         });
 
         commentArea[i].addEventListener('keyup', function(event) {
-            if (this.value == null || this.value == "") {
-                commentList[i].classList.remove('hidden');
-            } else {
+            if (/^([\\x30-\\x39]|[\\x61-\\x7a])$/i.test(event.key)) {
+                //console.log('char key');
                 commentList[i].classList.add('hidden');
                 setFormVal(this);
+            } else {
+                //console.log('non char');
+                if (this.value == null || this.value == "") {
+                    commentList[i].classList.remove('hidden');
+                }
             }
         });
 
@@ -50,7 +54,6 @@
         }
 
         commentList[i].addEventListener("click", function(event) {
-            //event.preventDefault();
             if (event.target.tagName === "LI") {
                 const selectedComment = event.target.textContent;
                 commentArea[i].value = selectedComment;
--- resources/views/partials/homework/homework-teacher-comment.blade.php
+++ resources/views/partials/homework/homework-teacher-comment.blade.php
@@ -5,7 +5,7 @@
     @else
     <flux:textarea row="4" column="25" id="comment-{{$index}}" class="w-xl block" label="Teacher's General Comment" />
     @endif
-    <ul class="list-group hidden p-2 w-xl" id="commentList-{{$index}}" style="cursor:pointer; background:grey;">
+    <ul class="list-group commentList hidden p-2 w-xl" id="commentList-{{$index}}" style="cursor:pointer; background:grey;">
         <li>太棒了!</li>
         <li>完全答對!</li>
         <li>再試一次!</li>
