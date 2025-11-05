a0cf7cc39b401e0f4ce5154c3d60c7bced224abf
Che Ming Yang <chemingyang@hotmail.com>
2025-11-05 02:34:27 -0500

    tweak

--- public/js/start-homework.js
+++ public/js/start-homework.js
@@ -132,7 +132,7 @@ function createListGroupItem(index, word, wordcount) {
     // Create span
     const span = document.createElement('span');
     span.textContent = `${index}. ${word}`;
-    span.className = 'flex px-2 py-1 text-center justify-center';
+    span.className = 'flex px-2 py-2 text-center justify-center text-xl';
     // Append span to div
     div.appendChild(span);
 
