b687b90e27d8febd74e825e4704b53865480644e
Che Ming Yang <chemingyang@hotmail.com>
2025-11-12 12:14:55 -0500

    minor tweak

--- resources/views/livewire/lesson-module/lesson-module-edit.blade.php
+++ resources/views/livewire/lesson-module/lesson-module-edit.blade.php
@@ -34,20 +34,19 @@
         />
         @if ($form->lessonmodule->audio)
         <div class="flex justify-between align-items-center gap-3">
-            <div class="flex items-center gap-2">
-                <audio id="player-edit" src="{{ asset('storage/' . $form->lessonmodule->audio) }}"></audio>
-                <flux:button size="xs" onclick="document.getElementById('player-edit').play()" class="p-0 border-0 bg-transparent">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z" />
-                    </svg>
+            <div class="flex item-center gap-2">
+                <audio id="player" src="{{ asset('storage/' . $form->lessonmodule->audio) }}"></audio>
+                <flux:button onclick="document.getElementById('player').play()" class="p-0 border-0 bg-transparent">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z" />
+                </svg>
                 </flux:button>
-                <span class="text-sm text-gray-400">Current audio</span>
             </div>
             <button type="button" 
                 @click="confirm('Are you sure you want to delete this audio?')" 
                 wire:click="deleteAudio({{ $form->lessonmodule->id }})" 
-                class="flex float-right text-xs font-semiboldtext-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-1 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+                class="flex float-right text-sm font-semiboldtext-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg px-2 py-2 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
             >Remove Audio</button>
         </div>
         @endif
@@ -70,7 +69,7 @@
                 <button type="button" 
                     @click="confirm('Are you sure you want to delete this image?')" 
                     wire:click="deleteImage({{ $form->lessonmodule->id }})" 
-                    class="justify-end text-xs font-semiboldtext-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-1 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+                    class="justify-end text-sm font-semiboldtext-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg px-2 py-2 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
                 >Remove Image</button>
             </div>  
         @endif
