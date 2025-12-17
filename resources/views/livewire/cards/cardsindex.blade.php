55d723dc95496f7434c245c02fb1a44592d4f56a
Che Ming Yang <chemingyang@hotmail.com>
2025-12-16 22:29:21 -0500

    work on cards

--- resources/views/livewire/cards/cardsindex.blade.php
+++ resources/views/livewire/cards/cardsindex.blade.php
@@ -1,16 +1,27 @@
 <section>
+    <div class="flex justify-end mr-6 pb-4">
+        <a href="{{ route('cards.index', ['offset' => $offset+1]) }}" class="text-indigo-500 hover:text-indigo-700 font-medium">
+        <span class="inline-flex mr-1">    
+            <label>Next Page</label>
+        </span>
+        </a>
+    </div>
     @foreach ($words as $word)
     <div class="flex">
         <flux:badge class="space-y-6 front inline-block">
             <div><span>{{ $word->level }}&nbsp;{{$word->type}}&nbsp;{{$word->subtype}}</span></div>
             <div><span style="font-size: 20px;">{{ $word->pinyin }}</span></div>
-            <div><span style="font-size: 48px;">{{ $word->traditional }}</span></div>
+            <div><span style="font-size: 64px;">{{ $word->traditional }}</span></div>
             <div><span>學華語向前走&nbsp;{{$word->book_id}}&nbsp;{{$word->lesson_id}}</span></div>
         </flux:badge>
         <flux:badge class="space-y-6 back inline-block">
             <div><span>{{ $word->category }}</span></div>
-            <div><span>{{ $word->english }}</span></div>
-            <div><span>{{ $word->qrcode }}</span></div>
+            <div><span style="font-size: 20px;">{{ $word->english }}</span></div>
+            @if (!empty($word->stroke_code))
+            <div><span style="font-size: 64px;"><img src="{{ asset('storage/stroke_code/' . $word->stroke_code.'.png') }}" alt="{{ $word->traditional }} stroke code" class="h-24 w-24 object-fit"></span></div>
+            @else
+            <div><span style="font-size: 64px;">&nbsp;</span></div>
+            @endif
             <div><span>學華語向前走&nbsp;{{$word->book_id}}&nbsp;{{$word->lesson_id}}</span></div>
         </flux:badge>
     </div>
