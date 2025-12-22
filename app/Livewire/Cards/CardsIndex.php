ea5ba7fb87edeb8491fddd515e064531b34efc22
Steve Che Ming Yang <chemingyang@hotmail.com>
2025-12-21 21:46:37 -0500

    try to merge it all (#9)
    * put card qr code stuff in queue set up imagick in dockerfile
    
    * add cards link
    
    * work on cards
    
    ---------
    
    Co-authored-by: Steve Yang <Steve Yang>

--- app/Livewire/Cards/CardsIndex.php
+++ app/Livewire/Cards/CardsIndex.php
@@ -3,15 +3,22 @@
 namespace App\Livewire\Cards;
 
 use Livewire\Component;
+use Livewire\Attributes\Url;
 use App\Models\Word;
 
 class CardsIndex extends Component
 {
     public $words;
+    #[Url]
+    public $offset = '0';
     
     public function mount()
     {
-        $this->words = Word::limit(10)->get();
+       
+        if (!is_numeric($this->offset) || intval($this->offset) < 0) {
+            $this->offset = '0';
+        }
+        $this->words = Word::limit(10)->offset(intval($this->offset)*10)->get();
     }
     
     public function render()
