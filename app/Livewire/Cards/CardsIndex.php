92122c4de65796ba236f5bd673afcffe0cbe066a
Che Ming Yang <chemingyang@hotmail.com>
2025-12-24 17:54:34 -0500

    change cards page so each page is only 2 cards for 1 word per pam request

--- app/Livewire/Cards/CardsIndex.php
+++ app/Livewire/Cards/CardsIndex.php
@@ -11,6 +11,7 @@ class CardsIndex extends Component
     public $words;
     #[Url]
     public $offset = '0';
+    private $step = 1;
     
     public function mount()
     {
@@ -18,7 +19,7 @@ class CardsIndex extends Component
         if (!is_numeric($this->offset) || intval($this->offset) < 0) {
             $this->offset = '0';
         }
-        $this->words = Word::limit(10)->offset(intval($this->offset)*10)->get();
+        $this->words = Word::limit($this->step)->offset(intval($this->offset)*$this->step)->get();
     }
     
     public function render()
